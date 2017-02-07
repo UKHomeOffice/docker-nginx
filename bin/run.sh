ea793b1a9d557185fa5afe8d3c76607f862d7cb2
Vaidas Jablonskis <jablonskis@gmail.com>
2017-02-07 14:13:10 +0000

    bin: force config file overwrite

--- bin/run.sh
+++ bin/run.sh
@@ -8,7 +8,7 @@ mkdir -p /run/nginx
 if [[ -f ${NGINX_CONFIG_FILE} ]]; then
   nginx -g 'daemon off;' -c ${NGINX_CONFIG_FILE}
 elif [[ -n ${NGINX_CONFIG} ]]; then
-  cp <(echo "${NGINX_CONFIG}") /etc/nginx/nginx.conf
+  cp -f <(echo "${NGINX_CONFIG}") /etc/nginx/nginx.conf
   nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
 else
   echo "[error] please set NGINX_CONFIG_FILE or NGINX_CONFIG variable."
