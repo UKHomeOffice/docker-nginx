0712dd862c587801a2f7ffdf13eae719215e2710
tasharnvb <tasharn.b@gmail.com>
2017-11-13 14:53:32 +0000

    Fixing the test script

--- ci-test-build.sh
+++ ci-test-build.sh
@@ -4,6 +4,23 @@
 
 set -x
 
+# Removes the test container and image
+clean_up() {
+  if docker ps -a --filter "name=nginx-test" | grep nginx-test &>/dev/null; then
+    echo "Removing nginx-test container..."
+    docker stop nginx-test && docker rm nginx-test &>/dev/null
+  else
+    echo "No nginx-test container found."
+  fi
+
+  if docker images nginx-test-image | grep nginx-test-image &>/dev/null; then
+    echo "Removing nginx-test-image image..."
+    docker rmi nginx-test-image &>/dev/null
+  else
+    echo "No nginx-test-image image found."
+  fi
+}
+
 clean_up
 
 # Build a new image and run a container with the new image
@@ -17,23 +34,7 @@ docker ps -a --filter "name=nginx-test" --filter "status=exited" | grep nginx-te
 # Test the container/image
 sleep 3
 #apk update && apk add curl
-curl -sf http://$$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx-test):10080
+curl -sf http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx-test):10080
 
 clean_up
 
-# Removes the test container and image
-clean_up() {
-  if docker ps -a --filter "name=nginx-test" | grep nginx-test &>/dev/null; then
-    echo "Removing nginx-test container..."
-    docker stop nginx-test && docker rm nginx-test &>/dev/null
-  else
-    echo "No nginx-test container found."
-  fi
-
-  if docker images nginx-test-image | grep nginx-test-image &>/dev/null; then
-    echo "Removing nginx-test-image image..."
-    docker rmi nginx-test-image
-  else
-    echo "No nginx-test-image image found."
-  fi
-}
